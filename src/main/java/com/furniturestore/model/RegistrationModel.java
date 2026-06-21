ec6f7ac8569c0a0aea71234fdb931dd437be5bb1
Damian Figueroa <133738308+Nemmyroo@users.noreply.github.com>
2026-06-21 10:21:49 -0700

    Update RegistrationModel.java added First and Last name
    Added private variables for first and last name

--- src/main/java/com/furniturestore/model/RegistrationModel.java
+++ src/main/java/com/furniturestore/model/RegistrationModel.java
@@ -6,6 +6,12 @@ import jakarta.validation.constraints.Size;
 
 public class RegistrationModel
 {
+	@NotBlank(message = "First name is required")
+    private String firstName;
+
+    @NotBlank(message = "Last name is required")
+    private String lastName;
+    
 	@NotBlank(message="Email is a required field")
 	@Email(message = "Please enter a valid email address")
 	private String email;
@@ -18,6 +24,42 @@ public class RegistrationModel
 	@Size(min=1, max=32, message="Password must be between 1 and 32 characters")
 	private String confirmPassword;
 	
+	/**
+	 * Simple getter method that will return the First name.
+	 * 
+	 * @return String with email
+	 */
+	public String getfirstName() {
+		return firstName;
+	}
+	
+	/**
+	 * Simple Setter that will set the First Name to the String in the parameters
+	 * @param name
+	 */
+	public void setFirstName(String firstName)
+	{
+		this.firstName = firstName;
+	}
+
+	/**
+	 * Simple getter method that will return the Last name.
+	 * 
+	 * @return String with email
+	 */
+	public String getLastName() {
+		return lastName;
+	}
+	
+	/**
+	 * Simple Setter that will set the First Name to the String in the parameters
+	 * @param name
+	 */
+	public void setLastName(String lastName)
+	{
+		this.lastName = lastName;
+	}
+
 	/**
 	 * Simple getter method that will return the email.
 	 * 
