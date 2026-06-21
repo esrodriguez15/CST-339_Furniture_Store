61cb858dfc076a6abbc487b9840ff132701bc842
Damian Figueroa <133738308+Nemmyroo@users.noreply.github.com>
2026-06-21 10:01:27 -0700

    Update RegistrationController.java
    Updated RegistrationController to make page function properly.

--- src/main/java/com/furniturestore/controller/RegistrationController.java
+++ src/main/java/com/furniturestore/controller/RegistrationController.java
@@ -1,14 +1,46 @@
 package com.furniturestore.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import com.furniturestore.model.RegistrationModel;
+
+import jakarta.validation.Valid;
 
 @Controller
 public class RegistrationController 
 {
 	@GetMapping("/registration")
-	public String registration() 
+	 public String displayLogin(Model model)
+    {
+        model.addAttribute("registrationModel", new RegistrationModel());
+        return "registration";
+    }
+
+	@PostMapping("/registration/doRegistration")
+	public String doRegistration(
+	        @Valid @ModelAttribute("registrationModel") RegistrationModel registrationModel,
+	        BindingResult bindingResult,
+	        Model model)
 	{
-		return "registration";
+	    if (!registrationModel.getPassword()
+	            .equals(registrationModel.getConfirmPassword()))
+	    {
+	        bindingResult.rejectValue(
+	                "confirmPassword",
+	                "password.mismatch",
+	                "Passwords do not match");
+	    }
+
+	    if(bindingResult.hasErrors())
+	    {
+	        return "registration";
+	    }
+
+	    model.addAttribute("successMessage", "Account created successfully.");
+	    return "registration";
 	}
 }
