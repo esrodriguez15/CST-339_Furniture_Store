e2e02e5652df7cdf86e30d0d3f4f3c1bc34b8cea
Damian Figueroa <133738308+Nemmyroo@users.noreply.github.com>
2026-06-21 10:00:28 -0700

    Create RegistrationModel.java
    Created Registration Model used for verifying registration parameters.

---
+++ src/main/java/com/furniturestore/model/RegistrationModel.java
@@ -0,0 +1,72 @@
+package com.furniturestore.model;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class RegistrationModel
+{
+	@NotBlank(message="Email is a required field")
+	@Email(message = "Please enter a valid email address")
+	private String email;
+	
+	@NotBlank(message="Password is a required field")
+	@Size(min=1, max=32, message="Password must be between 1 and 32 characters")
+	private String password;
+	
+	@NotBlank(message="Please confirm your password")
+	@Size(min=1, max=32, message="Password must be between 1 and 32 characters")
+	private String confirmPassword;
+	
+	/**
+	 * Simple getter method that will return the email.
+	 * 
+	 * @return String with email
+	 */
+	public String getEmail()
+	{
+		return email;
+	}
+	
+	/**
+	 * Simple getter method that will return the password
+	 * 
+	 * @return String with password
+	 */
+	public String getPassword()
+	{
+		return password;
+	}
+	
+	/**
+	 * Simple Setter that will set the email to the String in the parameters
+	 * @param name
+	 */
+	public void setEmail(String email)
+	{
+		this.email = email;
+	}
+	
+	/**
+	 * Simple Setter that will set the password to the String in the parameters.
+	 * @param password
+	 */
+	public void setPassword(String password)
+	{
+		this.password = password;
+	}
+
+	/**
+	 * @return the confirmPassword
+	 */
+	public String getConfirmPassword() {
+		return confirmPassword;
+	}
+
+	/**
+	 * @param confirmPassword the confirmPassword to set
+	 */
+	public void setConfirmPassword(String confirmPassword) {
+		this.confirmPassword = confirmPassword;
+	}
+}
