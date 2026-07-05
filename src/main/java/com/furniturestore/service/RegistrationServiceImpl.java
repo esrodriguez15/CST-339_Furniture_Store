ee1163e3ed6990b94e7bb2148c08078a0e8b8e89
Damian Figueroa <133738308+Nemmyroo@users.noreply.github.com>
2026-07-05 09:21:53 -0700

    Update RegistrationServiceImpl.java
    Check if email exists so there are no duplicate accounts made. Then converts model to entity to be saved to database.

--- src/main/java/com/furniturestore/service/RegistrationServiceImpl.java
+++ src/main/java/com/furniturestore/service/RegistrationServiceImpl.java
@@ -1,22 +1,41 @@
-/**
- * 
- */
 package com.furniturestore.service;
 
 import org.springframework.stereotype.Service;
 
+import com.furniturestore.data.entity.UserEntity;
+import com.furniturestore.data.service.UserDataService;
 import com.furniturestore.model.RegistrationModel;
 
-/**
- * 
- */
 @Service
-public class RegistrationServiceImpl implements RegistrationServiceInterface {
+public class RegistrationServiceImpl implements RegistrationServiceInterface
+{
+    private final UserDataService userDataService;
 
-	@Override
-	public boolean validateRegistration(RegistrationModel model) {
-		// hardwiredregistration logic
-		return model.getPassword().equals(model.getConfirmPassword());
-	}
+    public RegistrationServiceImpl(UserDataService userDataService)
+    {
+        this.userDataService = userDataService;
+    }
 
+    @Override
+    public boolean registerUser(RegistrationModel model)
+    {
+        // Check if email already exists
+        if (userDataService.findByEmail(model.getEmail()).isPresent())
+        {
+            return false;
+        }
+
+        // Convert model -> entity
+        UserEntity user = new UserEntity();
+        user.setFirstName(model.getFirstName());
+        user.setLastName(model.getLastName());
+        user.setEmail(model.getEmail());
+        user.setPhoneNumber(model.getPhoneNumber());
+        user.setPassword(model.getPassword());
+
+        // Save to DB
+        userDataService.save(user);
+
+        return true;
+    }
 }
