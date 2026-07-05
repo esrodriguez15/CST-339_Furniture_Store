50e2356466c562de05c65f448e8f1689bda2903f
Damian Figueroa <133738308+Nemmyroo@users.noreply.github.com>
2026-07-05 09:26:52 -0700

    Update UserRepository.java

--- src/main/java/com/furniturestore/data/repository/UserRepository.java
+++ src/main/java/com/furniturestore/data/repository/UserRepository.java
@@ -9,4 +9,9 @@ import com.furniturestore.data.entity.UserEntity;
 public interface UserRepository extends CrudRepository<UserEntity, Long>
 {
 	Optional<UserEntity> findByEmailAndPassword(String email, String password);
+	
+	/*
+	 * This will be used to check if the email is already registered.
+	 */
+	Optional<UserEntity> findByEmail(String email);
 }
